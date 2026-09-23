60f1272637ad43227d0ef6f461ffa74e50cac55a
Love <Lovetyagi659@gmail.com>
2026-09-23 22:38:24 +0530

    Add database name logging

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,18 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(
+            `MongoDB Connected: ${conn.connection.host}`
+        );
+
+        console.log(
+            `Database Name: ${conn.connection.name}`
+        );
     } catch (error) {
-        console.error(`MongoDB Connection Error: ${error.message}`);
+        console.error(
+            `MongoDB Connection Error: ${error.message}`
+        );
+
         process.exit(1);
     }
 };
